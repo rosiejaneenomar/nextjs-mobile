c6cb0eca63298e4b33af912d0c9511516d0d4592
Rosie Jane Enomar <rosie>
2024-02-20 14:14:01 +0800

    test

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,7 +21,6 @@ export default function Home() {
             rel="noopener noreferrer"
           >
             By{" "}
-            TEST
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
